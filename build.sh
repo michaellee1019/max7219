da7c115ce1cddb133788310742bf9dc00f431c69
Michael Lee <michaellee1019@gmail.com>
2024-04-12 13:39:23 -0400

    fix filename, add gitignore

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ apt-get install -y python3.11-venv
 python3 -m venv .venv
 . .venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m PyInstaller --onefile --hidden-import="googleapiclient" --hidden-import="viam-wrap" main.py
+python3 -m PyInstaller --onefile --hidden-import="googleapiclient" --hidden-import="viam-wrap" models.py
 tar -czvf dist/archive.tar.gz dist/main
